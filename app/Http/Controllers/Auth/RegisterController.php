609f2fb2b3859835a9d2bdd6e81143e226670a23
UmerNaeem <umer@techibits.com>
2025-06-17 21:12:48 +0500

    Random assignment on registeration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Models\Question;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -64,10 +65,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
+        $user = User::create([
+            'name'     => $data['name'],
+            'email'    => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    
+        $questionIds = Question::inRandomOrder()->limit(20)->pluck('id')->toArray();
+    
+        $user->questions()->attach($questionIds);
+    
+        return $user;
     }
 }
